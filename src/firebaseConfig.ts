8d35d7b061df904e14ddcec3d8cc6bbc2d195b1b
Madame-Aehm <emilyrachelstickler@gmail.com>
2024-08-06 10:15:41 +0200

    firebase config

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -6,13 +6,21 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID
+// };
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  apiKey: "AIzaSyAWRMGQr1N62tAM9oDnLc23G054RHI3vjc",
+  authDomain: "eagles-47f14.firebaseapp.com",
+  projectId: "eagles-47f14",
+  storageBucket: "eagles-47f14.appspot.com",
+  messagingSenderId: "220420106875",
+  appId: "1:220420106875:web:e441348becd59abe7b50de"
 };
 
 // Initialize Firebase
