cdede9cdd288eb5aa98cd42883a053f17b8f8516
Madame-Aehm <emilyrachelstickler@gmail.com>
2024-08-06 10:50:46 +0200

    remove all hosting before spike

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -3,25 +3,23 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
-};
 // const firebaseConfig = {
-//   apiKey: "AIzaSyAWRMGQr1N62tAM9oDnLc23G054RHI3vjc",
-//   authDomain: "eagles-47f14.firebaseapp.com",
-//   projectId: "eagles-47f14",
-//   storageBucket: "eagles-47f14.appspot.com",
-//   messagingSenderId: "220420106875",
-//   appId: "1:220420106875:web:e441348becd59abe7b50de"
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID
 // };
+const firebaseConfig = {
+  apiKey: "AIzaSyAqbanPMJMAqc6G_F55UcEtRoOkzxe1ACY",
+  authDomain: "eagles-5d693.firebaseapp.com",
+  projectId: "eagles-5d693",
+  storageBucket: "eagles-5d693.appspot.com",
+  messagingSenderId: "672585550344",
+  appId: "1:672585550344:web:765f5df0092ef41c129fbd"
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
